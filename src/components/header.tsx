c682915f3f7711267871c9fc0f10b3e51c4b8751
João Pedro Santiago <joao@joaosantiago.com.br>
2024-05-27 15:37:03 -0300

    update: empty project

--- src/components/header.tsx
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-    return (
-        <div className="w-full h-11 flex justify-center items-center text-center">
-            <span className="text-white text-4xl font-bold large:text-2xl small:text-lg">I'm going to work on my project for</span>
-        </div>
-    )
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,25 +6,13 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "#1F2023",
-        card: "#2A282D",
-        gray: "#7C7C8A",
-        red: "#AB212E",
-        green: "#00875F",
-        "light-green": "#09684D",
-      },
+      colors: {},
       fontFamily: {
         "roboto-mono": ["Roboto Mono", "monospace"],
         "roboto": ["Roboto", "sans-serif"]
       }
     },
-    screens: {
-      small: { max : "430px" },
-      medium: { min: "430px", max: "600px" },
-      large: { min: "600px", max: "750px" }
-    }
+    screens: {}
   },
   plugins: [],
 }
-
